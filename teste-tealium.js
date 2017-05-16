76d9a7fc8633a329bae2af7db47d4b294a7e7fbc
br.okamoto <br.okamoto@gmail.com>
2017-05-16 14:56:55 -0300

    teste
    teste

--- teste-tealium.js
+++ teste-tealium.js
@@ -1,8 +1,8 @@
 function testeTealium(){
-    var ctg = "Device Ready";
-    var act = "Device Ready";
-    var lbl = "Device Ready";
-    tealium.track("link", {"event_category" : ctg, "event_action" : act, "event_label" : lbl}, "tealium");
+	var ctg = "Device Ready";
+	var act = "Device Ready";
+	var lbl = "Device Ready";
+	tealium.track("link", {"event_category" : ctg, "event_action" : act, "event_label" : lbl}, "tealium");
 }
 
 document.addEventListener("deviceready", function() {
@@ -13,8 +13,8 @@ document.addEventListener("deviceready", function() {
 		tealium.track("link", {"event_category" : ctg, "event_action" : act, "event_label" : lbl}, "tealium");
 	});
 	var ctg = "Device Ready";
-    var act = "Device Ready";
-    var lbl = "Device Ready";
-    //utag.link({"event_category" : ctg, "event_action" : act, "event_label" : lbl});
-    tealium.track("link", {"event_category" : ctg, "event_action" : act, "event_label" : lbl}, "tealium");
+	var act = "Device Ready";
+	var lbl = "Device Ready";
+	//utag.link({"event_category" : ctg, "event_action" : act, "event_label" : lbl});
+	tealium.track("link", {"event_category" : ctg, "event_action" : act, "event_label" : lbl}, "tealium");
 }, false);
